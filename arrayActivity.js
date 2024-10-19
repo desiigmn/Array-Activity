5bfdaa7bc0f74b88db76c28382cba54cf9c9e066
Desiree Ann Guzman <desireeann000@gmail.com>
2024-10-19 18:47:43 +0800

    Commit with arrayActivity.js

--- arrayActivity.js
+++ arrayActivity.js
@@ -5,4 +5,7 @@ console.log("My first favorite singer is", favSingers[0]);
 // Favorite Numbers
 let favNumbers = ["12", "15", "04", "21"];
 let mixedArr = ["string", ["otherarray"], 123, true];
-console.log(mixedArr);
+console.log(mixedArr[0]);
+console.log(mixedArr[1]);
+console.log(mixedArr[2]);
+console.log(mixedArr[3]);
